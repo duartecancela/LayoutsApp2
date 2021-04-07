077ef189e325d0c75ac8653fffba285442825c9c
duartecancela <duartecancela@gmail.com>
2021-04-07 16:56:34 +0100

    Update activities

--- app/src/main/java/com/example/layoutsapp2/MainActivity.java
+++ app/src/main/java/com/example/layoutsapp2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.layoutsapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickConstrainToGuideline(View view) {
+        Intent intent = new Intent(this, ConstrainToGuidelineActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickConstrainToBarrier(View view) {
+        Intent intent = new Intent(this, ConstrainToBarrierActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickAdjustConstrainBias(View view) {
+        Intent intent = new Intent(this, AdjustConstraintBiasActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickLinearGroups(View view) {
+        Intent intent = new Intent(this, ControlLinearGroupsActivity.class);
+        startActivity(intent);
     }
 }
